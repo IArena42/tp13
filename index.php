a51c07d4d6c1d17ec5e3cdb82a6711f9d9b5f4fd
elihfitia-ops <elihfitia@gmail.com>
2026-06-10 11:21:06 +0300

    a

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <li><a href="departement.php"></a></li>
+<body>  
 </body>
 </html>
